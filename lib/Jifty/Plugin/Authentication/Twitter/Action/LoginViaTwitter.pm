f8a92003e316d770d7ef82c57bf014209357ce7d
Shawn M Moore <sartak@bestpractical.com>
2010-06-08 13:06:08 -0400

    Begin a new action

---
+++ lib/Jifty/Plugin/Authentication/Twitter/Action/LoginViaTwitter.pm
@@ -0,0 +1,39 @@
+use warnings;
+use strict;
+
+=head1 NAME
+
+Jifty::Plugin::Authentication::Twitter::LoginViaTwitter
+
+=cut
+
+package Jifty::Plugin::Authentication::Twitter::LoginViaTwitter;
+use base qw/Jifty::Action/;
+
+=head2 take_action
+
+Logs into the linked account, creating it if it does not already exist.
+
+=cut
+
+sub take_action {
+    my $self = shift;
+
+    return 1;
+}
+
+=head1 SEE ALSO
+
+L<Jifty::Plugin::Authentication::Twitter>
+
+L<Jifty::Action>, L<Jifty::CurrentUser>
+
+=head1 LICENSE
+
+Jifty is Copyright 2005-2010 Best Practical Solutions, LLC.
+Jifty is distributed under the same terms as Perl itself.
+
+=cut
+
+1;
+
